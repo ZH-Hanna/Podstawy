6af8ff2e6ed902ddfc634d92c54bd89322a56f54
ZH-Hanna <61236743+ZH-Hanna@users.noreply.github.com>
2020-04-22 00:54:34 +0300

    Добавьте файлы проекта.

---
+++ Delta/Delta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Delta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, x1, x2, delta;
+
+            try
+            {
+                Console.Write("Podaj wartość parametru a: ");
+                a = double.Parse(Console.ReadLine());
+                if (a == 0)
+                    throw new Exception();
+
+                Console.Write("Podaj wartość parametru b: ");
+                b = double.Parse(Console.ReadLine());
+
+                Console.Write("Podaj wartość parametru c: ");
+                c = double.Parse(Console.ReadLine());
+                delta = (Math.Pow(b, 2) - 4 * a * c);
+
+                if (delta > 0)
+                {
+                    x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    Console.WriteLine("Równanie ma dwa pierwiastki :");
+                    Console.WriteLine("     x1 =" + " " + x1);
+                    Console.WriteLine("     x2 =" + " " + x2);
+            }
+
+                else if (delta == 0)
+                {
+                    x1 = ((-b) / (2 * a));
+                    Console.WriteLine("Wyrażenie ma jeden pierwiastek {x1}");
+                }
+                else if (delta < 0)
+                    Console.WriteLine("Równanie nie ma pierwiastków rzeczywistych");
+            }
+            catch
+            {
+                Console.WriteLine("Parametr nie może być = 0 !");
+            }
+            Console.ReadKey();
+        }
+    }
+}
